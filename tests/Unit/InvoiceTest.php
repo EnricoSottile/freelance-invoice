685e6c8172518d67ceb290e46f161dee23b85381
Enrico <enrico.sottile@gmail.com>
2019-08-02 17:43:10 +0200

    added unit test

--- tests/Unit/InvoiceTest.php
+++ tests/Unit/InvoiceTest.php
@@ -7,17 +7,62 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use App\Models\Invoice;
+use Carbon\Carbon;
 
 class InvoiceTest extends TestCase
 {
+
+    /** 
+     * used for testing purposes
+     */
+    const INVOICE_NUMBER = 'TEST_INVOICE';
+
+
     /**
-     * A basic unit test example.
-     *
      * @return void
      */
     public function testInvoiceCanBeCreated()
     {
         $invoice = new Invoice();
-        $invoice->save();
+        $invoice->number = self::INVOICE_NUMBER;
+        $invoice->net_amount = rand(0 * 100, 10000 * 100) / 100;
+        $invoice->tax = rand(0 * 100, 100 * 100) / 100;
+        $invoice->description = 'This is a test...';
+        $invoice->date = null;
+        $invoice->sent_date = null;
+        $invoice->registered_date = null;
+
+        $this->assertTrue( $invoice->save() );
     }
+
+    /**
+     * @return void
+     */
+    public function testInvoiceCanBeUpdated()
+    {
+        $invoice = Invoice::where('number', self::INVOICE_NUMBER)->first();
+        
+        $val_before = $invoice->date;
+        $invoice->date = Carbon::now();
+        
+        $this->assertTrue( $invoice->save() );
+        
+        $invoice = Invoice::where('number', self::INVOICE_NUMBER)->first();
+        $this->assertTrue( $val_before !== $invoice->date );
+    }
+
+
+
+    /**
+     * @return void
+     */
+    public function testInvoiceCanBeDeleted()
+    {
+        $invoice = Invoice::where('number', self::INVOICE_NUMBER)->first();
+        
+        $this->assertTrue( $invoice->delete() );
+        $this->assertTrue( $invoice->forceDelete() );
+    }
+
+
 }
